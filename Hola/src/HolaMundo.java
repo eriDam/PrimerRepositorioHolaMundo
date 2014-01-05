1aaaeed725713a252593527736a3dbafd707b09c
Erika C <erika.c.caballero@gmail.com>
2014-01-05 13:17:20 +0100

    Tercer cambio
    Ejemplo simple de cadenas

--- Hola/src/HolaMundo.java
+++ Hola/src/HolaMundo.java
@@ -6,6 +6,15 @@ public class HolaMundo {
 		System.out.println("Hola Proyecto");
 		System.out.println("¿Que tal?");
 		System.out.println("Para ver los cambios ir a show in history");
+		System.out.println("-------Recordando ejemplos:");
+		
+		String s1 = "abcdef";
+		String s2 = s1 + "ghij"; //s2 contiene "abcdefghij"
+		String s3 = s1 + s2 + "klm"; //s3 contiene " abcdefabcdefghijklm"
+		System.out.println("s1="+s1);
+		System.out.println("s2="+s2);
+		System.out.println("Los dos juntos será: "+s3);
+		
 		
 	}
 
